521edacdb5164b42a272f004f85a79577e034924
Adam <adam.grzegorzewski@idemia.com>
2018-12-13 13:59:41 +0100

    Added ability to delete the cache and configuration from the application

--- src/menu/FileMenu.js
+++ src/menu/FileMenu.js
@@ -1,13 +1,46 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
+
+const options = {
+  type: 'question',
+  buttons: ['Yes, please', 'No!!!'],
+  defaultId: 1,
+  cancelId: 1,
+  title: 'Delete cache and config',
+  message: 'Do you want to do this?',
+  detail:
+    'Your configuration data and cache will be deleted.\nThe application will also be restarted.',
+};
 
 const FileMenu = {
   label: 'Menu',
   submenu: [
     {
-      label: 'Reload',
+      label: 'Reload App',
       accelerator: 'CmdOrCtrl+R',
       click: () => {
         BrowserWindow.getFocusedWindow().webContents.reloadIgnoringCache();
+      },
+    },
+    {
+      label: 'Delete cache and config',
+      accelerator: 'CmdOrCtrl+D',
+      click: () => {
+        var mainWindow = BrowserWindow.getFocusedWindow();
+        dialog.showMessageBox(
+          new BrowserWindow({
+            show: false,
+            alwaysOnTop: true,
+          }),
+          options,
+          response => {
+            if (response == 0) {
+              mainWindow.webContents.session.clearStorageData();
+              mainWindow.webContents.session.clearCache(function() {});
+              app.relaunch();
+              app.exit(0);
+            }
+          }
+        );
       },
     },
     {
