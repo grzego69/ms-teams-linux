937be380559769f4ba849fdea44e0a2b42794953
Adam <adam.grzegorzewski@idemia.com>
2018-12-13 10:27:40 +0100

    Changed user agent to the MS Edge

--- src/background.js
+++ src/background.js
@@ -56,7 +56,7 @@ app.on('ready', () => {
   checkUpdate();
 
   mainWindow.webContents.setUserAgent(
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    'Mozilla/5.0 (X11, Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240'
   );
 
   mainWindow.loadURL('https://teams.microsoft.com/');
