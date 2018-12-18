a5649fc88ba3b8ee8b155d2b0a84086d946160f8
Adam <adam.grzegorzewski@idemia.com>
2018-12-18 13:09:08 +0100

    Reload on blocking error

--- src/ms_t_preload.js
+++ src/ms_t_preload.js
@@ -1,1 +1,32 @@
+import { ipcRenderer } from 'electron';
+
+const blockingError = "TypeError: Cannot read property 'send' of undefined";
+
 require('electron-notification-shim')();
+
+// define a new console
+var console = (function(oldCons) {
+  return {
+    log: function(text) {
+      oldCons.log(text);
+    },
+    info: function(text) {
+      oldCons.info(text);
+    },
+    warn: function(text) {
+      oldCons.warn(text);
+    },
+    error: function(text) {
+      if (text) {
+        oldCons.error(text);
+      }
+
+      if (text && text.includes(blockingError)) {
+        ipcRenderer.send('errorInWindow');
+      }
+    },
+  };
+})(window.console);
+
+//Then redefine the old console
+window.console = console;
