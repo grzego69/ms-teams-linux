4c6e2e63cc1d15546e052de75856f8dbebf8af19
Adam <adam.grzegorzewski@idemia.com>
2019-01-02 17:39:04 +0100

    Changed method of popup creation (clear cache)

--- src/menu/FileMenu.js
+++ src/menu/FileMenu.js
@@ -2,17 +2,6 @@ import { app, BrowserWindow, dialog } from 'electron';
 
 const spawn = require('child_process').spawn;
 
-const options = {
-  type: 'question',
-  buttons: ['Yes, please', 'No!!!'],
-  defaultId: 1,
-  cancelId: 1,
-  title: 'Delete cache and config',
-  message: 'Do you want to do this?',
-  detail:
-    'Your configuration data and cache will be deleted.\nThe application will also be restarted.',
-};
-
 const FileMenu = {
   label: 'Menu',
   submenu: [
@@ -28,33 +17,42 @@ const FileMenu = {
       accelerator: 'CmdOrCtrl+D',
       click: () => {
         var mainWindow = BrowserWindow.getFocusedWindow();
+
         dialog.showMessageBox(
-          new BrowserWindow({
-            show: false,
-            alwaysOnTop: true,
-          }),
-          options,
+          mainWindow,
+          {
+            type: 'question',
+            buttons: ['Yes, please', 'No!!!'],
+            defaultId: 1,
+            cancelId: 1,
+            title: 'Delete cache and config',
+            message: 'Do you want to do this?',
+            detail:
+              'Your configuration data and cache will be deleted.\nThe application will also be restarted.',
+          },
           response => {
             if (response == 0) {
-              mainWindow.webContents.session.clearStorageData();
-              mainWindow.webContents.session.clearCache(function() {});
-              var currentFile = process.env.APPIMAGE;
-              var currentFileEsc = currentFile.replace(/'/g, "'\\''");
-              var cmd = `( exec '${currentFileEsc}' ) & disown $!`;
-              mainWindow.hide();
-              app.quit();
-              spawn(
-                '/bin/bash',
-                [
-                  '-c',
-                  `while ps ${process.pid} >/dev/null 2>&1; do kill -9 ${
-                    process.pid
-                  }; sleep 0.1; done; ${cmd}`,
-                ],
-                {
-                  detached: true,
-                }
-              );
+              if (response == 0) {
+                mainWindow.webContents.session.clearStorageData();
+                mainWindow.webContents.session.clearCache(function() {});
+                var currentFile = process.env.APPIMAGE;
+                var currentFileEsc = currentFile.replace(/'/g, "'\\''");
+                var cmd = `( exec '${currentFileEsc}' ) & disown $!`;
+                mainWindow.hide();
+                app.quit();
+                spawn(
+                  '/bin/bash',
+                  [
+                    '-c',
+                    `while ps ${process.pid} >/dev/null 2>&1; do kill -9 ${
+                      process.pid
+                    }; sleep 0.1; done; ${cmd}`,
+                  ],
+                  {
+                    detached: true,
+                  }
+                );
+              }
             }
           }
         );
