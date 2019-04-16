ec0dc21867fc7976af1854cf3007933c2d578c0f
Adam <adam.grzegorzewski@idemia.com>
2019-04-16 11:34:40 +0200

    Fixed disappearing annotation

--- src/mainApp.js
+++ src/mainApp.js
@@ -97,7 +97,7 @@ app.on('ready', () => {
   });
 
   mainWindow.webContents.setUserAgent(
-    'Mozilla/5.0 (Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/71.0.3578.98 Edge/19.17763'
+    'Mozilla/5.0 (Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3657.0 Safari/537.36'
   );
 
   mainWindow.loadURL('https://teams.microsoft.com/');
