40e2f22a6a113ccd7d4193ee3e880bd4f6e45eb5
Adam <adam.grzegorzewski@idemia.com>
2019-04-17 14:36:01 +0200

    Injected settings for calling using new userAgent

--- src/preload.js
+++ src/preload.js
@@ -33,3 +33,48 @@ var console = (function(oldCons) {
 
 //Then redefine the old console
 window.console = console;
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+
+    setTimeout(() => {
+      let injector = angular.element(document).injector();
+
+      if (injector) {
+        enableChromeVideoAudioMeetings(injector);
+        disablePromoteStuff(injector);
+
+        injector.get('settingsService').settingsService.refreshSettings();
+
+      }
+      // Future tests can be done in here...
+      // angular.element(document).injector().get('settingsService').appConfig.replyBoxFocusAfterNewMessage = true;
+      //last I look is enableIncomingVideoUnsupportedUfd groing from down to up.
+    }, 3000);
+  },
+);
+
+function enableChromeVideoAudioMeetings(injector) {
+  injector.get('callingSupportService').oneOnOneCallingEnabled = true;
+  injector.get('callingSupportService').isChromeMeetingSingleVideoEnabled = true;
+  injector.get('callingSupportService').isChromeVideoOneOnOneEnabled = true;
+  injector.get('callingSupportService').isChromeVideoMultipartyEnabled = true;
+  injector.get('settingsService').appConfig.enableCallingChromeOneOnOne = true;
+  injector.get('settingsService').appConfig.callingEnableChromeMeetingSingleVideo = true;
+  injector.get('settingsService').appConfig.callingEnableChromeMultipartyVideo = true;
+  //Screen sharing isn't implemented yet
+  //injector.get('settingsService').appConfig.enableChromeScreenSharing = true;
+  injector.get('settingsService').appConfig.enableAddToChatButtonForMeetings = true;
+  injector.get('settingsService').appConfig.enableSharingOnlyCallChrome = true;
+  injector.get('settingsService').appConfig.enableScreenSharingToolbar = true;
+  injector.get('settingsService').appConfig.enableCallingScreenPreviewLabel = true;
+  injector.get('settingsService').appConfig.callingEnableChromeOneToOneVideo = true;
+}
+
+function disablePromoteStuff(injector) {
+  injector.get('settingsService').appConfig.promoteMobile = false;
+  injector.get('settingsService').appConfig.promoteDesktop = false;
+  injector.get('settingsService').appConfig.hideGetAppButton = true;
+  injector.get('settingsService').appConfig.enableMobileDownloadMailDialog = false;
+}
